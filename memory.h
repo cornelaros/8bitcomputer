89e1e98ff620d194b9c606e5524483d851b9c92c
cornelaros <64271909+cornelaros@users.noreply.github.com>
2024-06-27 15:40:50 +0200

    jmp %2 program into mem

--- memory.h
+++ memory.h
@@ -25,23 +25,24 @@ class Memory
     uint8_t _clock_state;
 
     uint8_t _memory_address;
+    
     uint8_t _memory_content[16]{
-      NOP,      // 0000  0
-      JMP|13,   // 0001  1
-      NOP,      // 0010  2
-      JMP|10,   // 0011  3
-      JMP|6,    // 0100  4
-      0,        // 0101  5
-      JMP|15,   // 0110  6
-      JMP|2,    // 0111  7
-      0,        // 1000  8
-      0,        // 1001  9
-      NOP,      // 1010 10
-      NOP,      // 1011 11
-      JMP|0,    // 1100 12
-      JMP|4,    // 1101 13
-      0,        // 1110 14
-      JMP|7     // 1111 15
+      JMP|2,
+      NOP,
+      JMP|4,
+      NOP,
+      JMP|6,
+      NOP,
+      JMP|8,
+      NOP,
+      JMP|10,
+      NOP,
+      JMP|12,
+      NOP,
+      JMP|14,
+      NOP,
+      JMP|0,
+      NOP
     };
 };
 
